06a97ce6bf19ef4f015a5434d6a1dd4fcaa7dd7a
Matt Melito <matt_melito@outlook>
2023-02-10 12:51:42 -0800

    Add HTML form for user number input, with hidden error message.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,31 @@
 /*Business Logic*/
+window.onload = function() {
 
-let count = [];
+  Form.onsubmit = function(event) {
+  event.preventDefault();
+  let count = [];
 
-function countToNumber(userNumber) {
-  for (let i = userNumber; i >= 0; i--) {
-    count.push(i);
+    function countToNumber(userNumber) {
+      for (let i = userNumber; i >= 0; i--) {
+        count.push(i);
+      }
+      count.reverse();
+      console.log(count);
+    }
+
+    function numReplace(count) {
+      for (let i = 0; i < count.length; i++) {
+        if (String(count[i]).includes("3")) {
+          count[i] = "Won't you be my neighbor?";
+        }
+        else if (String(count[i]).includes("2")) {
+          count[i] = "Boop!";
+        }
+        else if (String(count[i]).includes("1")) {
+          count[i] = "Beep!";
+        }
+      }
+      console.log(count);
+    }
   }
-  count.reverse();
-  console.log(count);
-}
-
-function numReplace(count) {
-  for (let i = 0; i < count.length; i++) {
-    if (String(count[i]).includes("3")) {
-      count[i] = "Won't you be my neighbor?";
-    }
-    else if (String(count[i]).includes("2")) {
-      count[i] = "Boop!";
-    }
-    else if (String(count[i]).includes("1")) {
-      count[i] = "Beep!";
-    }
-  }
-  console.log(count);
-}
+};
